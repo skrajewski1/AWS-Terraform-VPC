204bf141bf9af2d11717dfb1d22619329a8e75e5
skrajewski1 <sean.krajewski@bonitahealthcare.com>
2024-05-22 20:24:05 -0500

    jenkinsfile2

--- variable.tf
+++ variable.tf
@@ -1,11 +1,17 @@
 variable "ENVIRONMENT" {
-  
+  description = "The environment for deployment"
+  type        = string
+  default     = "dev"
 }
 
 variable "REGION" {
-  
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
-  
+  description = "The type of instance to use"
+  type        = string
+  default     = "t2.medium"
 }
